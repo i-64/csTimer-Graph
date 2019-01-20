565c5b8248c0f1853684e75a1544a7f7e57f8415
Mrunal Ahire <mrunal.ahire@gmail.com>
2019-01-20 16:20:43 +0530

    files uppload

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express"),
+    app = express(),
+    upload = require("express-fileupload"),
+    fs = require('fs');
+
+//listen at port 8081
+app.listen(8081);
+
+// server started message
+console.log("Server started at port 3000");
+
+app.use(upload());
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+//create uploads directory if it doesn't exist
+if(!fs.existsSync('./uploads')){
+    fs.mkdirSync('./uploads');
+}
+
+//handle post request
+app.post("/upload",function(req,res){
+
+    if(req.files.upfile){
+
+        var file = req.files.upfile;
+        //var filename = file.name;
+        file.mv("./uploads/generated.csv",function(err){
+
+            //if error occured in uploading
+            if(err){
+                //print error
+                res.send("Error occured.");
+                console.log(err);
+            }
+            else{
+                //else success
+                res.send("Successful");
+            }
+        });
+    }
+});
